827e518304fa96f76435fa5d6fba145ad48ea4c8
egamovhamid <egamovhamid@gmail.com>
2023-09-05 22:12:47 +0500

    Refactored

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -39,10 +40,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: ReorderableListView(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         onReorder: (int oldIndex, int newIndex) {
@@ -75,7 +72,14 @@ class _MyHomePageState extends State<MyHomePage> {
               child: SizedBox(
                 height: 80,
                 child: Center(
-                  child: Text('Card ${_items[index]}'),
+                  child: Text(
+                    '${_items[index]}',
+                    style: const TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
             )
